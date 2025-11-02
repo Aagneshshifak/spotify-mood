fd482dfb4dbf1da6598eb9672832c56f9b8f91c1
Aagnesh shifak <167229723+Aagneshshifak@users.noreply.github.com>
2025-11-02 19:49:40 +0530

    Initialize frontend project with Vite and React setup, including essential configurations, dependencies, and initial components.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Player from "./components/Player";
+import Home from "./pages/Home";
+import MoodPlaylist from "./pages/MoodPlaylist";
+import Library from "./pages/Library";
+import Login from "./pages/Login";
+
+function App() {
+   return(
+    <div className="min-h-screen bg-gray-950 text-white flex flex-col">
+      <Navbar />
+      <div className="flex-1 overflow-y-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mood" element={<MoodPlaylist />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+       <Player />
+    </div>
+   );
+}
+
+export default App
